fcfde70cd57879ff12ddc8d42cceb8d3e355e49f
Alexey <mr.alexv2000@mail.ru>
2022-11-15 07:48:21 +0500

    Решение 1 задачи с семинара

---
+++ Task 01/Program_01.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 2 числа: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+int number_2 = Convert.ToInt32(Console.ReadLine());
+if (number_1 * number_1 == number_2){
+    Console.WriteLine($"Число {number_1} является квадратом числа {number_2}");
+}
+if (number_2 * number_2 == number_1){
+    Console.WriteLine($"Число {number_2} является квадратом числа {number_1}");
+}
+else {
+     Console.WriteLine("Квадрат из любого вписаного числа не равняется другому числу");
+}
