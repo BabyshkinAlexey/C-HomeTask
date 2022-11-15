5372d7b7f856ffe5fda034b42b4c6e2f9e10d135
Alexey <mr.alexv2000@mail.ru>
2022-11-15 09:43:59 +0500

    Решение 7 задания

---
+++ Task 07/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = number % 10;
+Console.WriteLine($"Число {digit} является последней цифрой числа {number}");
