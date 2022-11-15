ad007f0f13e4100634a3530a833f50906c4d3d78
Alexey <mr.alexv2000@mail.ru>
2022-11-15 09:15:04 +0500

    Решение 5 задания

---
+++ Task 05/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0){
+    int number0 = number * (-1);
+    for (int i = number0; i <= number; i++){
+        Console.Write(i + " ");
+    }
+}
+else if (number < 0){
+    int number0 = number * (-1);
+    for (int i = number; i <= number0; i++){
+        Console.Write(i + " ");
+    }
+}
+else if (number == 0) Console.WriteLine("0");
