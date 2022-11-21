78a857db434394f608e9b2bbf79e43b0b66275ad
Alexey <mr.alexv2000@mail.ru>
2022-11-21 15:28:39 +0500

    Решение 11 задачи

---
+++ Task 11/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное 3-ёх значное число  --> {number}");
+int RemuveDigit(int num)
+{
+    int digit1 = num / 100;
+    int digit2 = num / 10 % 10;
+    int digit3 = num % 10;
+    int result = digit1 * 10 + digit3;
+    return result; 
+}
+//Console.WriteLine($"{digit1}{digit3}");
+Console.WriteLine(RemuveDigit(number));
