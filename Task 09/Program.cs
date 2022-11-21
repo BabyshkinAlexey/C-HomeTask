4ba8cabc1d15a6a11f257c40dfe7a75ae2f702b2
Alexey <mr.alexv2000@mail.ru>
2022-11-21 15:00:17 +0500

    Дополнение к 9 задаче

--- Task 09/Program.cs
+++ Task 09/Program.cs
@@ -1,6 +1,18 @@
-﻿ int number = new Random().Next(10, 100);
- console.WriteLine($"Случайное число из отрезка 10 - 99 --> {number}");
- int firstdigit = number / 10;
- int seconddigit = number % 10;
- if (firstdigit > seconddigit) Console.WriteLine($"Наибольшая цифра --> {firstdigit}");
- else ($"Наибольшая цифра --> {seconddigit}");
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 --> {number}");
+// int firstdigit = number / 10;
+// int seconddigit = number % 10;
+// if (firstdigit > seconddigit) Console.WriteLine($"Наибольшая цифра --> {firstdigit}");
+// else Console.WriteLine($"Наибольшая цифра --> {seconddigit}");
+
+int MaxDigit(int num)
+{
+    int firstdigit = num / 10;
+    int seconddigit = num % 10;
+    if (firstdigit > seconddigit) return firstdigit;
+    return seconddigit;
+    // int max firstdigit > seconddigit ? firstdigit : seconddigit
+    // переменная = условие ? значение если истина : значение если ложь
+}
+int maxdigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра --> {maxdigit}");
