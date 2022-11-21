c97122d1df22b73070eb4c52856d8834afb23af0
Alexey <mr.alexv2000@mail.ru>
2022-11-21 18:05:14 +0500

    Решение 16 задачи

---
+++ Task 16/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите 1-ое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-ое число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool sqrtF(int num1, int num2)
+{
+    if (num1 * num1 == num2 || num2 * num2 == num1) return true;
+    return false;
+}
+Console.WriteLine(sqrtF(number1, number2) ? "YES" : "NO");
