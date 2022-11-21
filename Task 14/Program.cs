907da56b11d116b98fa08cac8ab5823330a08ccf
Alexey <mr.alexv2000@mail.ru>
2022-11-21 17:47:44 +0500

    Решение 14 задачи

---
+++ Task 14/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+bool multiplicity(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0) return true;
+    return false;
+}
+Console.WriteLine(multiplicity(number) ? "YES" : "NO");
