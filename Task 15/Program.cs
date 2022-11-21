6b6cc17f912fd13dee82c1f42c3b72e9dd3018ca
Alexey <mr.alexv2000@mail.ru>
2022-11-21 18:14:35 +0500

    Перезалив 15 задания

---
+++ Task 15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число, обозначающее день недели");
+int number = Convert.ToInt32(Console.ReadLine());
+bool weekend(int num)
+{
+    if (num == 6 || num == 7) return true;
+    return false;
+}
+Console.WriteLine(weekend(number) ? "YES" : "NO");
