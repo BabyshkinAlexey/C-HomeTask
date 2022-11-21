190ae35fecce07444a821fc05da23174163c87f4
Alexey <mr.alexv2000@mail.ru>
2022-11-21 17:13:12 +0500

    Выполнение 13 задания

---
+++ Task 13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите 3-ёх значное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digit(int num)
+{
+    if (num > 999){
+    {
+        while(num>999) num /= 10;
+    }
+    num %= 10;
+    return num;}
+    else if (num < 100) return 0;
+    else return num % 10;
+}
+if (digit(number) != 0) Console.WriteLine(digit(number));
+else Console.WriteLine("третьей цифры нет");
