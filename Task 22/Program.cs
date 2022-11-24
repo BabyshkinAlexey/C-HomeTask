87cb4e10d178bf8b0e10c2c575f8ad285a7a9f91
Alexey <mr.alexv2000@mail.ru>
2022-11-25 02:16:33 +0500

    Решение 22 задания

---
+++ Task 22/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 0)
+{
+    Console.WriteLine("Введите число больше 0");
+}
+void Qwadro(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine($"{i } --> { Math.Pow(i, 2)}");
+    }
+}
+Qwadro(number);
