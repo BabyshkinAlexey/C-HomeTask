842494f7adec57fa072870771d0ee676f3d7908d
Alexey <mr.alexv2000@mail.ru>
2022-11-25 00:13:47 +0500

    Решение 18 задачи

---
+++ Task 18/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите значение четверти");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string Range(int digit)
+{
+    if (digit == 1) return "x > 0 and y > 0";
+    if (digit == 2) return "x < 0 and y > 0";
+    if (digit == 3) return "x < 0 and y < 0";
+    if (digit == 4) return "x > 0 and y < 0";
+    return "введены некорректный номер четверти";
+}
+string range = Range(number);
+Console.WriteLine(range);
