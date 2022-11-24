f26f2f9cdf1ef9f60ed9af3e71bb4b1037267ef9
Alexey <mr.alexv2000@mail.ru>
2022-11-25 02:37:26 +0500

    Решени 23 задачи

---
+++ Task 23/Program.cs
@@ -0,0 +1,23 @@
+﻿//==================================================================================
+// от предыдущего задания различие в 1 символ, по этому сделал универсальную таблицу 
+//==================================================================================
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень числа: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+void control(int num, int pow)
+{
+    if (num <= 0) Console.WriteLine("Введите число больше 0");
+}
+
+
+void Qwadro(int num, int pow)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine($"{i } | { Math.Pow(i, pow)}");
+    }
+}
+control(number, degree);
+Qwadro(number, degree);
+
