fe130d25d2d4d987b6ba73e6219e330d4a602c8c
Alexey <mr.alexv2000@mail.ru>
2022-11-26 23:15:31 +0500

    Решение 26 задачи

---
+++ Task 26/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Quantity(int num)
+{
+    int result = 0;
+    while(num != 0)
+    {
+        num = num / 10;
+        result += 1;
+    }
+    return result;
+}
+int result = Quantity(number);
+if (result > 0) Console.Write($"В числе {number} количество цифр равно {result}");
+else if (number == 0) Console.Write($"В числе {number} количество цифр равно 1");
