9fb3c8a029976967858bf78076f4038d0dc9f061
Alexey <mr.alexv2000@mail.ru>
2022-11-27 00:27:04 +0500

    Решение 30 задачи

---
+++ Task 30/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+
+void RandomArr(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(0, 2);
+        Console.Write($"[{array[i]}] ");
+    }
+}
+RandomArr(array);
