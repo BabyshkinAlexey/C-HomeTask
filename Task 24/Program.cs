3d499e814b8c6866f5a60da0b43699b2707f931f
Alexey <mr.alexv2000@mail.ru>
2022-11-26 22:19:16 +0500

    Решение 24 задачи

---
+++ Task 24/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Summ(int num)
+{
+    int result = 0;
+    for(int i = 1; i <= num; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+int summ = Summ(number);
+
+if (summ <= 0)Console.WriteLine("Введите число больше 0");
+
+else Console.WriteLine($"Сумма чисел от 1 до {number} = {summ}");
