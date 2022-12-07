963d78806106ac76cc46a3d1b90666fad423ef77
Alexey <mr.alexv2000@mail.ru>
2022-12-07 19:19:22 +0500

    Решение 42 задачи

---
+++ Task 42/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] Binary(int num, int lenght)
+{
+    int[] arr = new int[lenght];
+    for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = num % 2;
+            num /= 2;
+            // Console.Write($"[{arr[i] }] ");
+        }
+    return arr;
+}
+
+int Size(int num)
+{
+    int length = 0;
+    while (num > 0)
+    {
+        num /= 2;
+        length ++;
+    }
+    return length;
+}
+
+void PrintArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"[{arr[i]}] ");
+        else Console.Write($"[{arr[i]}] ");
+    }
+}
+
+int length = Size(number);
+int[] result = Binary(number, length);
+Console.WriteLine($"Число {number} в двоичном представлении: ");
+Array.Reverse(result);
+PrintArr(result);
