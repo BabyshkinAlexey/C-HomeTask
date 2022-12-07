4073e7639954c703e0ba13f2206ca5fad407eb9a
Alexey <mr.alexv2000@mail.ru>
2022-12-07 18:39:42 +0500

    Решене 41 задачи

---
+++ Task 41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+Console.Write("Сколько будет чисел? ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArrayRndInt(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+int Counting(int[] arr)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) result ++;
+    }
+    return result;
+}
+
+int[] array = CreateArrayRndInt(size);
+int result = Counting(array);
+Console.Write($"Количество чисел больших 0 равняется: {result}");
