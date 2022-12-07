d751fdb779a0d5d1b4b0b8d081ce884c12ec0e25
Alexey <mr.alexv2000@mail.ru>
2022-12-07 18:13:04 +0500

    Решение 40 задачи

---
+++ Task 40/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-ое число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+bool TriangleInequality(int aT, int bT, int cT)
+{
+    if ((aT < bT + cT) && (bT < aT + cT) && (cT < aT + bT))
+    return true;
+    else return false;
+}
+
+if (TriangleInequality(a, b, c) == true) Console.WriteLine("Треугольник существует с сторонами такой длины");
+else Console.WriteLine("Треугольник НЕ существует с сторонами такой длины");
