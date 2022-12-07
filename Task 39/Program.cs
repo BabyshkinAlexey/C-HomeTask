78c629299cbcde79ee9b8f439b62631ee267580a
Alexey <mr.alexv2000@mail.ru>
2022-12-07 17:51:07 +0500

    Решение 39 задачи

---
+++ Task 39/Program.cs
@@ -0,0 +1,40 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write(" ]");
+    Console.WriteLine();
+}
+
+void ReverseArrey(int[] arr)
+{
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    }
+}
+
+int[] arr = CreateArrayRndInt(5, 1, 9);
+PrintArr(arr);
+ReverseArrey(arr);
+PrintArr(arr);
+// Array.Reverse(arr);
+// PrintArr(arr);
