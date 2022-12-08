e2135c4125d90d2036cde344546030f9df1090a1
Alexey <mr.alexv2000@mail.ru>
2022-12-09 02:43:54 +0500

    Решение 51 задачи

---
+++ Task 51/Program.cs
@@ -0,0 +1,47 @@
+﻿int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int SumDiagonalMatrix(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j) sum += matrix[i,j];
+        }
+    }
+    return sum;
+}
+
+int[,] arrey2D = CreateMatrixRndInt(3, 3, 1, 9);
+PrintMatrix(arrey2D);
+Console.WriteLine();
+int sumDiagonalMatrix = SumDiagonalMatrix(arrey2D);
+Console.Write($"Сумма эллементов, находящихся на главной диагонали = {sumDiagonalMatrix}");
