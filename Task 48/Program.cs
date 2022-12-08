702eca660311271ade98ea104eb72620c1a876ab
Alexey <mr.alexv2000@mail.ru>
2022-12-09 01:24:35 +0500

    Решение 48 задачи

---
+++ Task 48/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int[,] CreateMatrixInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+            else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] arrey2D = CreateMatrixInt(3, 4, -9, 9);
+PrintMatrix(arrey2D);
