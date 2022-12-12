e3bcd87d897661436965ddbe65a1e689792c1a11
Alexey <mr.alexv2000@mail.ru>
2022-12-12 23:01:15 +0500

    Решение 57 задачи

---
+++ Task 57/Program.cs
@@ -0,0 +1,67 @@
+﻿int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[] MatrixToArrey(int[,] matrix)
+{
+    int[] arr = new int[matrix.Length];
+    int k = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            arr[k++] = matrix[i, j];
+        }
+    }
+    return arr;
+}
+
+void CountValue(int[] arr)
+{
+    int count = 1;
+    int num = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] == num) count++;
+        else
+        {
+            Console.WriteLine($"{num} встречается {count} раз(a).");
+            count = 1;
+            num = arr[i];
+        }
+    }
+    Console.WriteLine($"{num} встречается {count} раз(a).");
+}
+
+int[,] arrey2D = CreateMatrixRndInt(3, 3, 1, 9);
+PrintMatrix(arrey2D);
+int[] arrey = MatrixToArrey(arrey2D);
+Array.Sort(arrey);
+
+Console.WriteLine();
+CountValue(arrey);
