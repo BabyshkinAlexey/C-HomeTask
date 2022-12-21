33fe7b90e07fd22a26aa6ee0e75961021de9fc69
Alexey <mr.alexv2000@mail.ru>
2022-12-21 20:30:18 +0500

    Решение 67 задачи

---
+++ Task 67/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+void SumNumbers (int num, int sum)
+{
+    int digit = 0;
+    if (num == 0)
+    {
+        if (sum < 0) sum = sum * (-1);
+        Console.WriteLine($"{sum}");
+        return;
+    }
+    digit = num % 10;
+    sum += digit;
+    num /= 10;
+    SumNumbers(num, sum);
+}
+
+SumNumbers(number, 0);
