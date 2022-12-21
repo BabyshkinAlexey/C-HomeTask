ff3fcc786e005debed6b71dbc005c043ecab4957
Alexey <mr.alexv2000@mail.ru>
2022-12-21 20:45:09 +0500

    Решение 68 задачи

---
+++ Task 68/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+int Ackermann(int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return Ackermann(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return Ackermann(numberM - 1, Ackermann(numberM, numberN - 1));
+    return Ackermann(numberM, numberN);
+}
+
+Console.WriteLine($"Функция Аккермана для чисел A({numberM},{numberN}) = {Ackermann(numberM, numberN)}");
