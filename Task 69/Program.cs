3e5f809522fcfd597fd8b5abffdd861678cb9072
Alexey <mr.alexv2000@mail.ru>
2022-12-21 20:56:37 +0500

    Решение 69 задачи

---
+++ Task 69/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 1-ое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-ое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+int DegreeNumbers(int num, int degree)
+{
+    return degree == 0 ? 1 : DegreeNumbers(num, degree - 1) * num;
+}
+
+Console.WriteLine(DegreeNumbers(number1, number2));
