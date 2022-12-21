a0c0feccd23fc0b8a38e7cdbcb7f860d8ceed3e0
Alexey <mr.alexv2000@mail.ru>
2022-12-21 18:59:23 +0500

    Решение 63 задачи

---
+++ Task 63/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    NaturalNumbers (num - 1);
+    Console.Write($"{num} ");
+}
+
+NaturalNumbers(number);
